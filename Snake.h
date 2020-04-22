3f3d8c92659424fb2424de82f587ef0bd0756445
Мария Страшенко <strash@mail.ru>
2020-04-22 13:09:13 +0300

    first commit

---
+++ Snake.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef _Snake_h_
+#define _Snake_h_
+#include "Reptiles.h"
+
+class TSnake : public TReptiles
+{
+public:
+    TSnake();
+    const char* Golos();
+    int teeth();
+    bool Shell();
+};
+
+
+#endif "# project1" 
