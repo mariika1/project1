de92293952bac78f473638115a3583c47bb98818
Мария Страшенко <strash@mail.ru>
2020-04-22 13:03:39 +0300

    first commit

---
+++ Crocodile.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef _Crocodile_h_
+#define _Crocodile_h_
+#include "Reptiles.h"
+
+class TCrocodile : public TReptiles
+{
+public:
+    TCrocodile();
+    const char* Golos();
+    int teeth();
+
+};
+
+
+#endif "# project1" 
+"# project1" 
