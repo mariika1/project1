623fd51ea3f2b134e5ee16060380b340ad7706b4
Мария Страшенко <strash@mail.ru>
2020-04-22 13:06:33 +0300

    first commit

---
+++ Koala.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef _Koala_h_
+#define _Koala_h_
+#include "Mammals.h" 
+
+class TKoala : public TMammals
+{
+public:
+	TKoala();
+	const char* Golos();
+	int Echo();
+	double Moloco();
+};
+
+#endif"# project1" 
