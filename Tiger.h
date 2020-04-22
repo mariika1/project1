7b153ebd255fbf0ee34a37cfced8e56a6fa0a2db
Мария Страшенко <strash@mail.ru>
2020-04-22 13:10:30 +0300

    first commit

---
+++ Tiger.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef __Tiger_h_
+#define __Tiger_h_
+#include "Mammals.h"
+
+class TTiger : public TMammals
+{
+public:
+	TTiger();
+	const char* Golos();
+	const char* Eating();
+	double Moloco();
+};
+#endif"# project1" 
